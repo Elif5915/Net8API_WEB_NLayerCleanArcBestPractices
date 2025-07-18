cbaa794965779e3edeb0a72c56d52c0550b59eec
Elif Yıldırım <elifyildirim3@outlook.com>
2025-07-18 23:52:25 +0300

    Add project files.

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App_Repositories;
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // App_Repositories katmanı altındaki IEntityTypeConfiguration implement eden tüm sınıfları al ef geç demiş olduk.
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App_Repositories.Extensions;
+public static class RepositoryExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            #region 1.yol
+            //builder.Services.AddDbContext<AppDbContext>(options =>
+            //{
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString(name: "SqlServer"));
+            //});
+            #endregion
+
+            var connectionString = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+            options.UseSqlServer(connectionString!.SqlServer);
+        });
+
+        return services;
+    }
+}
